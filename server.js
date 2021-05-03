8bf6a9216c2891e20f7ff2d4e7cab8aa1b2f89d2
bayan-alkhatib <bayankhatibtr@gmail.com>
2021-05-03 23:21:39 +0300

    lab 12

--- server.js
+++ server.js
@@ -10,22 +10,25 @@ const server=express();
 server.use(cors());
 server.set('view engine','ejs');
 server.use(express.static('./public'));
+server.use(express.urlencoded({extended:true}));
 
-
+// https://www.googleapis.com/books/v1/volumes?q=search+${q}:${search}&maxResults=10`
 function booksSearchHand (req,res){
-  let bookSearchInput=req.query.search_query;
-  let bookUrl=` https://www.googleapis.com/books/v1/volumes?q=${bookSearchInput}&maxResults=10`;
-  superagent.post(bookUrl)
+  let bookSearchInput=req.body.search_query;
+  
+  superagent.get(bookUrl)
     .then(booksData=>{
       let volumeInfoArr=booksData.body.items.volumeInfo;
       let newBookInstance= volumeInfoArr.map(element=>{
         return new BOOKS(element);
       });
-      if(newBookInstance.title===null ) newBookInstance.title='Not Found';
-      if (newBookInstance.author===null) newBookInstance.author='Not Found';
-      if(newBookInstance.descripition===null) newBookInstance.descripition='Not Found';
-      if( newBookInstance.image_url===null) newBookInstance.image_url= 'https://i.imgur.com/J5LVHEL.jpg';
-      res.render('searches/show',{bookinstance:newBookInstance});
+      console.log(volumeInfoArr);
+      // if(newBookInstance.title===null ) newBookInstance.title='Not Found';
+      // if (newBookInstance.author===null) newBookInstance.author='Not Found';
+      // if(newBookInstance.descripition===null) newBookInstance.descripition='Not Found';
+      // if( newBookInstance.image_url===null) newBookInstance.image_url= 'https://i.imgur.com/J5LVHEL.jpg';
+      res.render('Iam working');
+      res.render('pages/searches/show',{bookinstance:newBookInstance});
     })
     .catch(()=>{
       res.render('pages/error');
@@ -33,10 +36,6 @@ function booksSearchHand (req,res){
 }
 
 
-
-server.get('*', (req,res)=>{
-  res.render('pages/error');
-});
 
 server.get('/',(req,res)=>{
   res.render('pages/index');
@@ -46,7 +45,12 @@ server.get('/new',(req,res)=>{
   res.render('searches/new');
 });
 
-server.post('/searches',booksSearchHand);
+server.post('/searches',booksSearchHand );
+
+
+server.get('*', (req,res)=>{
+  res.render('pages/error');
+});
 
 
 function BOOKS(element) {
