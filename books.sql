59695055c502d28c1e398f07f011c1db47f25bdc
bayan-alkhatib <bayankhatibtr@gmail.com>
2021-05-04 03:36:34 +0300

    progress

--- books.sql
+++ books.sql
@@ -4,7 +4,7 @@ create table books (
     id SERIAL PRIMARY KEY,
     author VARCHAR(255),
     title VARCHAR(255),
-     description TEXT,
+    descriptions VARCHAR(255),
     isbn VARCHAR(255),
-    image_url VARCHAR(255
+    image_url VARCHAR(255)
 )
